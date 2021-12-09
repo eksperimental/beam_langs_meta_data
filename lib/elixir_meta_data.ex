ec0bbe01fe806dcff0829dee3894ec5b35314a19
Eksperimental <eksperimental@autistici.org>
2021-12-08 22:57:54 -0500

    Add files in priv/ as external resources

--- lib/elixir_meta_data.ex
+++ lib/elixir_meta_data.ex
@@ -4,13 +4,16 @@ defmodule ElixirMetaData do
   """
   import ElixirMetaData.Util
 
+  @external_resource "priv/releases.json"
+  @external_resource "priv/compatibility.json"
+
   @type elixir_version_key :: String.t()
   @type otp_version_key :: non_neg_integer
 
   @releases priv_dir("releases.json") |> read_and_decode_json!()
 
+  # source "https://api.github.com/repositories/1234714/releases"
   @spec releases() :: [%{String.t() => term}]
-  @doc url: "https://api.github.com/repositories/1234714/releases"
   def releases(), do: @releases
 
   @compatibility priv_dir("compatibility.json") |> read_and_decode_json!()
