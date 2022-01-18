5e9a92b06a6ef4912e81060313c4f430b9d57ff0
Eksperimental <eksperimental@autistici.org>
2022-01-18 09:19:13 -0500

    Slightly update docs

--- lib/beam_langs_meta_data.ex
+++ lib/beam_langs_meta_data.ex
@@ -1,6 +1,8 @@
 defmodule BeamLangsMetaData do
   @moduledoc """
-  Provides meta-data for Elixir and Erlang/OTP.
+  Provides meta-data for BEAM languages.
+
+  So far the only two supported languages are Elixir and Erlang/OTP.
 
   The information in this module is regularly updated, and stored with every new release of this library.
   This library does not download information neither at compile time nor at real time.
@@ -140,7 +142,7 @@ defmodule BeamLangsMetaData do
   NOTE: The information that gets updated in real time is removed such as number of downloads and reactions.
   Currently `"updated_at"` is also removed, but it will be included when a mechanism to check for
   udpated in any part of the JSON files is implemented. As of now, only the new entries are added to this
-  functions, so if there is any correction in a previously entender entry it will not be updated.
+  functions, so if there is any correction in a previously entered entry it will not be updated.
 
   ## Examples
 
